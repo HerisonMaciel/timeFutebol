12e9fbfffd074ce03679badc44638d7f9038e37a
HerisonMaciel <herisson2maciel@hotmail.com>
2023-07-13 04:00:24 -0300

    estrutura do projeto

--- src/main/java/com/projeto/timeFutebol/repository/ClubeRepository.java
+++ src/main/java/com/projeto/timeFutebol/repository/ClubeRepository.java
@@ -1,4 +1,12 @@
 package com.projeto.timeFutebol.repository;
 
-public interface ClubeRepository {
+import com.projeto.timeFutebol.model.Clube;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ClubeRepository extends CrudRepository<Clube, long> {
+
+    List<Clube> findByName(String nome);
+
 }
--- src/main/java/com/projeto/timeFutebol/repository/JogadorRepository.java
+++ src/main/java/com/projeto/timeFutebol/repository/JogadorRepository.java
@@ -1,2 +1,12 @@
-package com.projeto.timeFutebol.repository;public interface JogadorRepository {
+package com.projeto.timeFutebol.repository;
+
+import com.projeto.timeFutebol.model.Jogador;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface JogadorRepository extends CrudRepository<Jogador, long> {
+
+    List<Jogador> findByName(String nome);
+
 }
