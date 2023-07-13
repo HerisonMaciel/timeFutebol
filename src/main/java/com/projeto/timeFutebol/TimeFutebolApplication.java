15799a7c1f880afafe91d80253b54133173dfb2b
HerisonMaciel <herisson2maciel@hotmail.com>
2023-07-13 01:44:08 -0300

    subindo projeto

---
+++ src/main/java/com/projeto/timeFutebol/TimeFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.timeFutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeFutebolApplication.class, args);
+	}
+
+}
